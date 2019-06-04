a0537e3231b491fb042a4d3bc1a60e8a0a78ff3c
James McCarthy <james2mccarthy@gmail.com>
2019-06-04 15:07:31 +0700

    Add a filter input that does nothing.

--- src/features/shifts/ShiftsList.jsx
+++ src/features/shifts/ShiftsList.jsx
@@ -8,6 +8,7 @@ function ShiftsList(props) {
   return (
     <div>
       <h1>Shifts List</h1>
+      <input placeholder='filter' />
       {
         list.map((shift) => (
           <div key={shift.roleId}>
